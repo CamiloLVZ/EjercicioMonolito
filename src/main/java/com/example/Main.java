9c718575764b2251e4942feaa483a4e2d2b5e1b2
Camilo <camilolon6@gmail.com>
2026-02-23 21:08:36 -0500

    eliminar service redundante

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -11,16 +11,16 @@ public class Main {
         CuentaController cuentaController = new CuentaController(JpaUtil.getEntityManagerFactory());
 
         try {
-            Cliente cliente1 = clienteController.crearCliente("Ana Torres", "CC-1001");
-            Cliente cliente2 = clienteController.crearCliente("Luis Díaz", "CC-1002");
+            Cliente cliente1 = clienteController.crearCliente("Ana Torres", "CC-10527");
+            Cliente cliente2 = clienteController.crearCliente("Luis Díaz", "CC-107272");
 
-            cuentaController.crearCuentaAhorros(cliente1.getId(), "AHO-0001", 500_000);
-            cuentaController.crearCuentaAhorros(cliente2.getId(), "AHO-0002", 300_000);
-            cuentaController.crearCuentaCredito(cliente1.getId(), "CRE-0001", 2_000_000);
+            cuentaController.crearCuentaAhorros(cliente1.getId(), "AHO-0001", 500000);
+            cuentaController.crearCuentaAhorros(cliente2.getId(), "AHO-0002", 300000);
+            cuentaController.crearCuentaCredito(cliente1.getId(), "CRE-0001", 2000000);
 
-            cuentaController.depositar("AHO-0001", 100_000);
-            cuentaController.transferir("AHO-0001", "AHO-0002", 150_000);
-            cuentaController.retirarAvanceCredito("CRE-0001", 200_000);
+            cuentaController.depositar("AHO-0001", 100000);
+            cuentaController.transferir("AHO-0001", "AHO-0002", 150000);
+            cuentaController.retirarAvanceCredito("CRE-0001", 200000);
 
             System.out.println("Clientes registrados: " + clienteController.listarClientes().size());
             System.out.println("Cuentas de ahorro registradas: " + cuentaController.listarCuentasAhorros().size());
